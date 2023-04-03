787db10b2f6a2425cfc242097dae468a9b6d18a1
Redjorm <redjorm12@gmail.com>
2023-04-03 16:55:31 -0400

    portafolio

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function darkMode () {
+  const navbarIconMoonHTML = document.querySelector('.navbar__icon-moon')
+  const navbarIconSunHTML = document.querySelector('.navbar__icon-sun')
+  const moonIconHTML = document.querySelector('.bx-moon')
+  const sunIconHTML = document.querySelector('.bxs-sun')
+  const bodyHTML = document.querySelector('body')
+
+  moonIconHTML.addEventListener('click', function () {
+    bodyHTML.classList.toggle('dark-theme')
+    navbarIconMoonHTML.classList.toggle('logo__darkmode')
+    navbarIconSunHTML.classList.toggle('logo__darkmode')
+
+    if (document.body.classList.contains('dark-theme')) {
+      localStorage.setItem('dark-mode', 'true')
+    } else {
+      localStorage.setItem('dark-mode', 'false')
+    }
+  })
+
+  sunIconHTML.addEventListener('click', function () {
+    bodyHTML.classList.toggle('dark-theme')
+    navbarIconMoonHTML.classList.toggle('logo__darkmode')
+    navbarIconSunHTML.classList.toggle('logo__darkmode')
+
+    if (document.body.classList.contains('dark-theme')) {
+      localStorage.setItem('dark-mode', 'true')
+    } else {
+      localStorage.setItem('dark-mode', 'false')
+    }
+  })
+}
+
+function saveDarkMode () {
+  const bodyHTML = document.querySelector('body')
+  const navbarIconMoonHTML = document.querySelector('.navbar__icon-moon')
+  const navbarIconSunHTML = document.querySelector('.navbar__icon-sun')
+
+  if (localStorage.getItem('dark-mode') === 'true') {
+    bodyHTML.classList.add('dark-theme')
+    navbarIconMoonHTML.classList.add('logo__darkmode')
+    navbarIconSunHTML.classList.remove('logo__darkmode')
+  } else {
+    bodyHTML.classList.remove('dark-theme')
+    navbarIconMoonHTML.classList.remove('logo__darkmode')
+    navbarIconSunHTML.classList.add('logo__darkmode')
+  }
+}
+
+function main () {
+    
+  darkMode()
+  saveDarkMode()
+
+}
+
+main()
